e57bd50141bbd6b855f4bc0fb2b7f432df85c73f
dudkodmytrii007 <dudkodmytrii@gmail.com>
2024-11-23 21:56:39 +0100

    colors

--- src/components/Header/ChatNav.vue
+++ src/components/Header/ChatNav.vue
@@ -33,13 +33,15 @@
 
 <template>
   <nav class="chat-nav">
-    <div class="chat-nav-header">
+    <div class="chat-nav-top">
       <h2 class="chat-nav-title">Messages</h2>
 
       <select name="myChats" id="myChats">
         <option value="all">All</option>
       </select>
     </div>
+
+    <input type="text" placeholder="Search" class="chat-nav-input">
 
     <ul class="chat-nav-list" v-for="chat in myChats" :key="chat.chatId">
       <ChatNavItem :chat="chat" />
@@ -54,7 +56,7 @@
     flex-direction: column;
   }
 
-  .chat-nav-header {
+  .chat-nav-top {
     display: grid;
     grid-template-columns: auto 80px;
     align-items: center;
@@ -74,6 +76,15 @@
 
   #myChats option {
     background-color: red;
+  }
+
+  .chat-nav-input {
+    width: 100%;
+    height: 40px;
+    border-radius: 12px;
+    background-color: var(--color-background-second);
+    border: none;
+    outline: none;
   }
 
   .chat-nav-list {
