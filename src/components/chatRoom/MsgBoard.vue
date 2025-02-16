805ba783e9b66a7d89862e3daa0098467d45fd82
dudkodmytrii007 <dudkodmytrii@gmail.com>
2025-02-16 20:28:34 +0100

    chat room - messages function

--- src/components/chatRoom/MsgBoard.vue
+++ src/components/chatRoom/MsgBoard.vue
@@ -1,25 +1,106 @@
 <script lang="ts" setup>
+  import { ref, onMounted, watch, nextTick } from 'vue';
+  import { useRoute } from 'vue-router';
+  import axiosInstance from '@/services/axiosInstance';
 
+  interface Message {
+    chatId: string;
+    authorId: string;
+    avatar: string;
+    name: string;
+    text: string;
+    date: Date;
+  }
+
+  const route = useRoute();
+  const myUserId = ref(localStorage.getItem("userId"));
+  const chatMsg = ref<Message[]>([]);
+  const page = ref(1);
+  const totalPages = ref(0);
+  const chatContainer = ref<HTMLElement | null>(null);
+  const loadingPage = ref(false);
+
+  async function getChatMsg() {
+    const urlChatId = route.params.chatId;
+
+    if (!urlChatId) {
+      console.error("Chat ID is missing or invalid");
+      return;
+    }
+
+    if (!myUserId.value) {
+      console.error("User ID is missing or invalid");
+      return;
+    }
+
+    try {
+      const response = await axiosInstance.post('chat/getChatMessages', {
+        "chatId": urlChatId,
+        "userId": myUserId.value,
+        "page": page.value,
+        "pageSize": 20
+      })
+
+      if (!loadingPage.value) {
+        chatMsg.value = response.data.messages;
+
+      }else {
+        const oldHeight = chatContainer.value?.scrollHeight || 0;
+        const oldScrollTop = chatContainer.value?.scrollTop || 0;
+
+        chatMsg.value.push(...response.data.messages);
+        await nextTick();
+
+        const newHeight = chatContainer.value?.scrollHeight || 0;
+        chatContainer.value!.scrollTop = oldScrollTop + (newHeight - oldHeight);
+
+        loadingPage.value = false;
+      }
+
+      totalPages.value = response.data.totalPages;
+
+    } catch(error) {
+      console.error("Error fetching chat messages:", error);
+    }
+  }
+
+  function onScroll() {
+    if (!chatContainer.value) return;
+
+    if (chatContainer.value.scrollTop === 0 && page.value < totalPages.value && !loadingPage.value) {
+      loadingPage.value = true;
+      page.value++;
+      getChatMsg();
+    }
+  }
+
+  onMounted(() => {
+    getChatMsg();
+  });
+
+  watch(() => route.params.chatId, async () => {
+    chatMsg.value = [];
+    page.value = 1;
+    await getChatMsg();
+  });
 </script>
 
 <template>
   <div class="shadow top-shadow"></div>
 
-  <ul>
-    <li class="received">
-      <img src="https://avatars.githubusercontent.com/u/14400871" alt="" class="avatar">
-      <span class="author">Friend</span>
-      <p class="message">Another message goes here.</p>
-      <p class="message">Yet another message from Friend.</p>
-      <p class="message">Final message from Friend.</p>
-    </li>
-
-    <li class="sent">
-      <img src="https://avatars.githubusercontent.com/u/76972913" alt="" class="avatar">
-      <span class="author">You</span>
-      <p class="message">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi eos nisi esse eius? Magni nostrum, doloremque, delectus enim doloribus ex dolor illo ratione deleniti sequi voluptate ipsa officia, nihil ullam optio incidunt quis veritatis aperiam architecto cum vero voluptatem rem debitis quae. Dignissimos blanditiis exercitationem eligendi placeat, unde expedita fugiat.</p>
-      <p class="message">Another message from You.</p>
-      <p class="message">Final message from You.</p>
+  <ul @scroll="onScroll" ref="chatContainer">
+    <li
+      v-for="msg in chatMsg"
+      :key="msg.chatId"
+      :class="{ received: msg.authorId !== myUserId, sent: msg.authorId === myUserId }"
+    >
+      <img
+        :src="msg.avatar"
+        :alt="msg.name"
+        class="avatar"
+      >
+      <span class="author">{{ msg.name }} | {{ msg.date }}</span>
+      <p class="message">{{ msg.text }}</p>
     </li>
   </ul>
 
